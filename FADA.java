d48ee730a997763877e24a1d33698e65f8c2334f
lclucumi <luz.lucumi@correounivalle.edu.co>
2019-04-18 10:12:42 -0500

    Update FADA.java

--- FADA.java
+++ FADA.java
@@ -19,11 +19,10 @@ public class FADA {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner sc = new Scanner(System.in);
+                Scanner sc = new Scanner(System.in);
 		int size = sc.nextInt();
 		int[][] m = new int[size][size];
 		int[][] s = new int[size][size];
-		int villas = 0;
 
 		for (int i = 0; i < size; i++) {
 			for (int j = 0; j < size; j++) {
@@ -31,47 +30,38 @@ public class FADA {
 
 			}
 		}
-		
 		for (int i = 0; i < size; i++) {
-			int j = 0, y = 0;
+			for (int j = 0; j < size; j++) {
 
-			/*
-			 * verifica si hay o no relacion amistosa con todos los paises que se encuentren alojados
-			 * hasta el momento
-			 */
-			while (i != j) {
-				if (s[y][j] == 1) {			//Primero verifica si hay un pais alojado en esa villa
-					if (m[i][j] == 1) {		//Comprueba si existen relaciones diplomaticas no amistosas
-						y++;				//Si las hay, cambia a la siguiente villa o crea una nueva
-						j = 0;				
-						if (y > villas)
-							villas = y;		//Lleva el conteo de villas creadas
-
+				int y = j;
+				int x = 0;
+				while (i != j) {
+					if (m[i][j] == 1) {
+						x = x + 1;
+						j++;
+						y = j;
 					} else {
 						j++;
+						y = j;
 					}
-				} else {
-					j++;
 				}
+				int contador=0; //nos permitirá comparar si las posiciones anteriores son o no aliadas
+                                while(contador<i+1){ //mientras se llega a la posición actual
+                                    if(m[contador][j]==0){ //si es aliado
+                                        s[contador][y]=1;
+                                        contador++;
+                                    }
+                                }
+				
+				s[x][y] = 1;
+				j = size;
 			}
-                        int contador=0; //nos permitirá comparar si las posiciones anteriores son o no aliadas
-                        while(contador<i+1){ //mientras se llega a la posición actual
-                            if(m[contador][j]==0){ //si es aliado
-                                s[contador][y]=1; //se ubica un 1 en la posición
-                                contador++;
-                            }
-                        }
-			//s[y][j] = 1;					//Ubica al pais en su villa correspondiente
 		}
-
-		//Muestra el numero de villas con sus huespedes
-		for (int i = 0; i <= villas; i++) {
+		for (int i = 0; i < size; i++) {
 			System.out.println();
 			for (int j = 0; j < size; j++) {
 				System.out.print(s[i][j] + " ");
 			}
 		}
-
 	}
-
 }
