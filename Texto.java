b7d9c4f33015ba9004670e9c7a70ab9995d016d3
lclucumi <luz.lucumi@correounivalle.edu.co>
2019-04-20 12:07:02 -0500

    Update Texto.java

--- Texto.java
+++ Texto.java
@@ -19,10 +19,19 @@ public class Texto {
      */
     public static void main(String[] args) {
        // TODO code application logic here
+	/*En nombre de fichero, coloquen el nombre del archivo txt donde está la matriz,
+	recuerden que para que la lectura sea válida, debe tener el mismo modo que los ejemplos,
+	Ejemplo:
+	4
+	0 1 0 1
+	1 0 1 0
+	0 1 0 1
+	1 0 1 0*/
         final String nomFichero="C:\\Users\\miime_000\\Documents\\NetBeansProjects\\FADA\\src\\fada\\M.txt";
         try(FileReader fr=new FileReader (nomFichero)){
                 int valor=fr.read();
                 String tamano="";
+                /*Se lee el primer valor, para saber el tamaño de los arreglos m y s respectivamente*/
                 if(valor!=48 && valor!=49){
                     tamano = Character.toString((char)valor);
                 }
@@ -30,30 +39,41 @@ public class Texto {
 		int[][] m = new int[size][size];
 		int[][] s = new int[size][size];
                 char[][] matriz= new char[size][size];
-                System.out.print("//////////////MATRIZ INGRESADA//////////////\n");
+                /*Continuación de lectura*/
                 valor=fr.read();
-                for (int i = 0; i < size; i++) {
-                    for (int j = 0; j < size; j++) {
+                //for (int i = 0; i < size; i++) {
+                //    for (int j = 0; j < size; j++) {
+                        int x=0,y=0;
+                        /*Mientras el archivo contenga valores, se leerá cada posición*/
                         while(valor!=-1){
-                            //System.out.print((char)valor);
-                            matriz[i][j]=(char)valor;
-                            valor=fr.read();
+                            /*en ASCII 32 es el espacio, así que no es necesario tenerlo en cuenta*/
+                            if(valor!=32){
+                                if(y<size){
+                                    matriz[x][y]=((char)valor);
+                                    y++;
+                                    valor=fr.read();
+                                }
+                                /*En caso de haber llegado a la posición horizontal final*/
+                                else{
+                                    y=0;
+                                    x++;
+                                }
+                            }
+                            /*Continuamos con la lectura de los datos que siguen en el archivo*/
+                            valor=fr.read(); 
                         }
-                    }
-                }
-            System.out.print("\n\n/////////MATRIZ M///////////////\n");
+                    //}
+                //}
+            System.out.print("\n\n/////////MATRIZ m///////////////\n");
+            /*impresión de m*/
             for (int i=0; i<size;i++){
                 System.out.println("");
                 for(int j=0; j<size; j++){
-                    System.out.print(matriz[i][j]);
+                    System.out.print(matriz[i][j]+" ");
                 }
             }
-            //System.out.print(matriz+"\n");
         }catch(IOException e){
             System.out.println("Problemas con el E/S "+e);
         }
-    }
-    
+    }  
 }
-
-        
