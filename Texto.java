895f43f28e270ba66a325a432da969c7ac65eb90
lclucumi <luz.lucumi@correounivalle.edu.co>
2019-04-21 12:39:26 -0500

    Update Texto.java

--- Texto.java
+++ Texto.java
@@ -19,6 +19,14 @@ public class Proyecto {
      */
     public static void main(String[] args) {
        // TODO code application logic here
+        /*la variable nomFichero tendrá la ruta donde se encuentre el txt con el arreglo,
+        para que se lea correctamente debe estar como en los ejemplos:
+        5
+        0 1 0 1 0
+        1 0 1 0 1
+        0 1 0 1 0
+        1 0 1 0 1
+        0 1 0 1 0*/
         final String nomFichero="C:\\Users\\miime_000\\Documents\\NetBeansProjects\\FADA\\src\\fada\\M.txt";
         try(FileReader fr=new FileReader (nomFichero)){
                 int valor=fr.read();
@@ -32,33 +40,31 @@ public class Proyecto {
 		int[][] s = new int[size][size];
                 /*Continuación de lectura*/
                 /*Saltos para evitar leer espacios en blanco*/
-                valor=fr.read();
-                valor=fr.read();
-                valor=fr.read();
-                //for (int i = 0; i < size; i++) {
-                    //for (int j = 0; j < size; j++) {
-                        int x=0,y=0;
-                        /*Mientras el archivo contenga valores, se leerá cada posición*/
-                        while(valor!=-1){
-                            /*en ASCII 32 es el espacio, así que no es necesario tenerlo en cuenta*/
-                            if(valor!=32){
-                                if(y<size){
-                                    m[x][y]=Character.getNumericValue((char)valor);
-                                    y++;
-                                    valor=fr.read();
-                                }
-                                /*En caso de haber llegado a la posición horizontal final*/
-                                else{
-                                    y=0;
-                                    x++;
-                                }
+                for(int i=0;i<3;i++){
+                    valor=fr.read();
+                }
+                int x=0,y=0;
+                /*Mientras el archivo contenga valores, se leerá cada posición*/
+                while(valor!=-1){
+                        /*en ASCII 32 es el espacio, así que no es necesario tenerlo en cuenta*/
+                        if(valor!=32){
+                            if(y<size){
+                                /*como los valores están en ASCII (char) los convertimos a enteros*/
+                                m[x][y]=Character.getNumericValue((char)valor);
+                                y++;
+                                /*se continúa con la lectura de archivo*/
+                                valor=fr.read();
                             }
-                            /*Continuamos con la lectura de los datos que siguen en el archivo*/
-                            valor=fr.read(); 
+                            /*En caso de haber llegado a la posición horizontal final*/
+                            else{
+                                y=0;
+                                x++;
+                            }
                         }
-                    //}
-                //}
-            System.out.print("\n\n/////////MATRIZ m///////////////\n");
+                    /*Continuamos con la lectura de los datos que siguen en el archivo*/
+                    valor=fr.read(); 
+                }
+            System.out.print("/////////MATRIZ m///////////////\n");
             /*impresión de m*/
             for (int i=0; i<size;i++){
                 System.out.println("");
